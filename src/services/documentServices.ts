be36f3bca793b95ff9909e11c94a0af44e3a65c0
Nelev <smacchia.mattia@gmail.com>
2026-03-07 06:48:55 +0100

    feat: add compose yaml and phi3 model

--- src/services/documentServices.ts
+++ src/services/documentServices.ts
@@ -10,14 +10,14 @@ export const processDocument = async (): Promise<any> => {
      const response = await fetch(`${modelUrl}/api/generate`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        model: "llama3.2",
+        body: JSON.stringify({
+        model: process.env.AI_MODEL_NAME,
         prompt: extractPrompt,
         stream: false,
         images: [imageBase64]
-      })
+  })
     });
-    const data = await response.json();  ;
+    const data = await response.json();
     return data;
 } catch (error: any) {
     throw new Error(error.message);
